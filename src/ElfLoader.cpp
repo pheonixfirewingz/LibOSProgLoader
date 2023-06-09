f7dacc49f16e7da3d09cdd2860ea83169bb7ee87
Phoenixfirewingz <lukethemodded@gmail.com>
2023-06-09 16:17:24 +0100

    Add checks to elf loader

--- src/ElfLoader.cpp
+++ src/ElfLoader.cpp
@@ -19,7 +19,7 @@ static inline losResult readElf64SectionHeader(Elf64SectionHeader& header,const 
 	return LOS_SUCCESS;
 }
 
-static inline losResult readElf64Header(Elf64Header& header, const char* file_data) noexcept
+static inline losResult readElf64Header(Elf64Header& header, const char* file_data,const size_t& file_size) noexcept
 {
 	try
 	{
@@ -36,6 +36,32 @@ static inline losResult readElf64Header(Elf64Header& header, const char* file_da
 			losPrintError("[LIBOS::ELF_LOADER]: tried to load a none x86_64 elf file we only support x86_64 right now");
 			return LOS_ERROR_COULD_NOT_INIT;
 		}
+		if (((const Elf64Header*)file_data)->e_version == 0)
+		{
+			losPrintError("[LIBOS::ELF_LOADER]: tried to load a elf file with bad elf version");
+			return LOS_ERROR_COULD_NOT_INIT;
+		}
+		if (((const Elf64Header*)file_data)->e_ident.ei_data_encoding != 1)
+		{
+			losPrintError("[LIBOS::ELF_LOADER]: tried to load a elf file has big enitan or corupted elf data encoding this is a no no");
+			return LOS_ERROR_COULD_NOT_INIT;
+		}
+		if (((const Elf64Header*)file_data)->e_ident.ei_abi_version != 0)
+		{
+			losPrintError("[LIBOS::ELF_LOADER]: tried to load a elf file with bad abi elf version");
+			return LOS_ERROR_COULD_NOT_INIT;
+		}
+		if (((const Elf64Header*)file_data)->e_type != Elf64Type::EXEC && ((const Elf64Header*)file_data)->e_type != Elf64Type::DYN && 
+			((const Elf64Header*)file_data)->e_type != Elf64Type::REL && ((const Elf64Header*)file_data)->e_type != Elf64Type::CORE)
+		{
+			losPrintError("[LIBOS::ELF_LOADER]: tried to load a elf file with an none supported type loader expected REL, EXEC, DYN or CORE");
+			return LOS_ERROR_COULD_NOT_INIT;
+		}
+		if (((const Elf64Header*)file_data)->e_phoff > file_size || ((const Elf64Header*)file_data)->e_shoff > file_size) {
+			losPrintError("[LIBOS::ELF_LOADER]: AHHHHH! corupted elf header pointer files");
+			return LOS_ERROR_COULD_NOT_INIT;
+		}
+		//TODO/FIXME: need to add all safety checks for the spec
 		memcpy(&header, file_data, sizeof(Elf64Header));
 	}
 	catch (...)
@@ -64,7 +90,7 @@ losResult loseExec(_in_ const loseExecInfo exec_info)
 			return res;
 	}
 	Elf64Header header{};
-	if (losResult res = readElf64Header(header,read_str.get()); res != LOS_SUCCESS)
+	if (losResult res = readElf64Header(header,read_str.get(),read_str_size); res != LOS_SUCCESS)
 		return res;
 	if (header.e_shoff == 0)
 	{
